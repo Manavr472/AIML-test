dac8bd0667dbfe811c2c0d005763692421fc690c
Manavr472 <manavr472@gmail.com>
2023-07-20 19:11:22 +0530

    updated practice.py

--- Practice.py
+++ Practice.py
@@ -15,7 +15,7 @@ training_images=training_images/255.0
 test_images=test_images/255.0
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(1024, activation=tf.nn.relu),
+  tf.keras.layers.Dense(28, activation=tf.nn.relu),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
